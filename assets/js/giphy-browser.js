c4f658a58ae3cb019de7a4b5335aab9c2ea9cd25
Wynn Blevins <wynnblevins@yahoo.com>
2018-02-28 16:46:52 -0500

    Added hard coded array of animal strings to giphy-browser.js and wrote code to load the animals on page load.

--- assets/js/giphy-browser.js
+++ assets/js/giphy-browser.js
@@ -1,5 +1,22 @@
 (function () {
     'use strict';
     
-    var animals = [];
+    var API_KEY = 'Sa9egT2O8QKTXw9j87UZM9BcTQ5NxjMv';
+    var animals = ['Squirrel', 'Cat', 'Dog', 'Crab', 'Chimpanzee', 'Aligator',
+        'Rat', 'Turtle', 'Snake', 'Penguin', 'Ostrich', 'Zebra', 'Skunk', 
+        'Pig', 'Gorilla', 'Shark', 'Blue Whale', 'Aardvark', 'Cow', 'Horse', 
+        'Antelope', 'Lion', 'Tiger', 'Bear', 'Velocorapter', 'Eagle', 'Sheep',
+        'Yeti', 'Jackalope', 'Sasquatch', 'Chupacabra', 'Baboon', 'Ermine', 'Fox',
+        'Hedgehog', 'Iguana', 'Jackrabbit', 'Killer Whale', 'Lemur', 'Mongoose'
+    ];
+
+    function initAnimals() {
+        var $animalElements = $('#gif-buttons');
+        
+        animals.forEach(function (animal) {
+            $animalElements.append('<button type="button" class="btn btn-default">' + animal + '</button>');
+        });
+    }
+
+    initAnimals();
 })();
