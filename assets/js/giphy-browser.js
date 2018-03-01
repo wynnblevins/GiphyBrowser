d183b274948397677109a2c6792243433e64ebe8
Wynn Blevins <wynnblevins@yahoo.com>
2018-02-28 19:36:59 -0500

    Users can now interact with rest api by entering text to search on into the search box and hitting the submit button.

--- assets/js/giphy-browser.js
+++ assets/js/giphy-browser.js
@@ -1,7 +1,8 @@
-(function () {
-    'use strict';
-    
-    var API_KEY = 'Sa9egT2O8QKTXw9j87UZM9BcTQ5NxjMv';
+'use strict';
+
+$(document).ready(function () {
+    var BASE_URL = 'https://api.giphy.com/v1/gifs/search?q=';
+    var API_KEY = '&api_key=Sa9egT2O8QKTXw9j87UZM9BcTQ5NxjMv';
     var animals = ['Squirrel', 'Cat', 'Dog', 'Crab', 'Chimpanzee', 'Aligator',
         'Rat', 'Turtle', 'Snake', 'Penguin', 'Ostrich', 'Zebra', 'Skunk', 
         'Pig', 'Gorilla', 'Shark', 'Blue Whale', 'Aardvark', 'Cow', 'Horse', 
@@ -9,6 +10,33 @@
         'Yeti', 'Jackalope', 'Sasquatch', 'Chupacabra', 'Baboon', 'Ermine', 'Fox',
         'Hedgehog', 'Iguana', 'Jackrabbit', 'Killer Whale', 'Lemur', 'Mongoose'
     ];
+
+    function createRowDiv() {
+        return $('<div class="row"></div>');
+    }
+
+    function onDataReturn(responseData) {
+        var gifData = null, imageString = null, row = null;
+        
+        for (var i = 0; i < responseData.data.length; i++) {
+            if (i % 4 === 0) {
+                row = createRowDiv();
+                $('#search-results').append(row);
+            }
+        
+            var $col = $('<div class="col-sm-3"><div>');
+            row.append($col);
+            
+            imageString = '<img class="img-responsive" src="' + 
+            responseData.data[i].images.fixed_height_still.url + '"/>'
+        
+            $col.append(imageString);
+        }
+    }
+
+    function createURL(searchString) {
+        return BASE_URL + searchString + API_KEY;
+    }
 
     function initAnimals() {
         var $animalElements = $('#gif-buttons');
@@ -21,8 +49,24 @@
     }
 
     $('#submit-button').click(function () {
-        console.log('hello world');
+        // get url 
+        var searchString = $('#add-animal-input').val();
+        
+        // format it the way giphy likes
+        var url = createURL(searchString);
+
+        $.ajax({ 
+            type: "GET",
+            dataType: "json",
+            url: url,
+            success: function(data){        
+                onDataReturn(data);
+            }, 
+            error: function () {
+                alert('Unable to grab gifs :-(');
+            }
+        });
     });
 
     initAnimals();
-})();
+});
